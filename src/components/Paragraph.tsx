319ba9efb78224284f2af47b772e2997a0d9839f
Jörgen Thelin <thelin.jorgen@gmail.com>
2023-03-01 14:30:39 +0100

    p font size and truncate

--- src/components/Paragraph.tsx
+++ src/components/Paragraph.tsx
@@ -1,5 +1,21 @@
-const Paragraph = ({ children }: { children: React.ReactNode }) => {
-  return <p className="mb-4 font-zonapro zonapro">{children}</p>;
+const Paragraph = ({
+  children,
+  variant = "normal",
+  truncate,
+}: {
+  children: React.ReactNode;
+  variant?: "normal" | "small";
+  truncate?: boolean;
+}) => {
+  return (
+    <p
+      className={`mb-4 font-zonapro zonapro${
+        variant === "normal" ? "" : " text-sm"
+      } ${truncate && " truncate ..."}`}
+    >
+      {children}
+    </p>
+  );
 };
 
 export default Paragraph;
---
+++ src/stories/Paragraph.stories.tsx
@@ -0,0 +1,35 @@
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { Paragraph } from "..";
+
+export default {
+  title: "UI/Paragraph",
+  component: Paragraph,
+  parameters: {
+    layout: "fullscreen",
+  },
+  decorators: [(Story) => <div className="max-w-xl p-8">{<Story />}</div>],
+} as ComponentMeta<typeof Paragraph>;
+
+const Template: ComponentStory<typeof Paragraph> = ({ children, ...args }) => (
+  <Paragraph {...args}>{children}</Paragraph>
+);
+
+export const Default = Template.bind({});
+Default.args = {
+  children:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi dignissimos, sapiente deserunt mollitia aliquid repudiandae soluta atque totam molestiae, odio repellat eos possimus placeat illo veniam iste. Dolorem, consequuntur sequi!",
+};
+
+export const Truncate = Template.bind({});
+Truncate.args = {
+  truncate: true,
+  children:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi dignissimos, sapiente deserunt mollitia aliquid repudiandae soluta atque totam molestiae, odio repellat eos possimus placeat illo veniam iste. Dolorem, consequuntur sequi!",
+};
+
+export const SmallText = Template.bind({});
+SmallText.args = {
+  variant: "small",
+  children:
+    "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi dignissimos, sapiente deserunt mollitia aliquid repudiandae soluta atque totam molestiae, odio repellat eos possimus placeat illo veniam iste. Dolorem, consequuntur sequi!",
+};
