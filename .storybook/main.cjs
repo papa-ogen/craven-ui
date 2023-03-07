3056de0bfc8ca91a03d3cb782b1a4b5af59941ce
Jörgen Thelin <jorthe@carnegie.se>
2023-03-07 15:55:42 +0100

    removed

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,5 +1,3 @@
-const ASSET_PATH = process.env.ASSET_PATH || "/";
-
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -14,8 +12,5 @@ module.exports = {
   },
   features: {
     storyStoreV7: false,
-  },
-  output: {
-    publicPath: ASSET_PATH,
   },
 };
